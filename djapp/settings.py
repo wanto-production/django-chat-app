fc2a3bdb9f23dffd59e60a89cf14ddb89db70dd9
wanto-production <ikwansatria3974@gmail.com>
2024-09-21 10:29:13 +0700

    updtaed

--- djapp/settings.py
+++ djapp/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +58,7 @@ STATICFILES_DIRS = [
     BASE_DIR /'myapp/static',
 ]
 
-NPM_BIN_PATH = "C:/Program Files (x86)/Yarn/bin/yarn.cmd"
+NPM_BIN_PATH = os.getenv("YARN_DIR")
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
